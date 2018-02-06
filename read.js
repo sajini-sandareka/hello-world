28743ab8b897b2837e86ff870382cc0e6e5107c0
sajini-sandareka <ssdayananda@virtusa.com>
2018-02-06 07:31:36 +0000

    Update read.js
    i added comments

--- read.js
+++ read.js
@@ -1,7 +1,7 @@
 var fs=require('fs');
 var readme=fs.readFileSync('file.txt','uft8');
-console.log(readme);
+console.log(readme);//this synchronize file reading
 
 var readme=fs.readFile('file.txt','uft8', function(err,content){
 console.log(content);
-});
+});//this asynchronize file reading. non-blocking
